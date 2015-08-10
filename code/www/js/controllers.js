f33a6402a3758c6ddf19b3a144e33472c8eecee5
Chris Brody <chrisbrody@codifyacademy.com>
2015-08-10 16:52:39 -0400

    added init function

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -5,36 +5,14 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 Controller for the discover page
 */
 .controller('DiscoverCtrl', function($scope, $timeout, User, Recommendations) {
-	Recommendations.getNextSongs()
-	  .then(function() {
-	  	$scope.currentSong = Recommendations.queue[0];
-	  });
-
-  // $scope.songs = [
-	// 	{
-  //        "title":"Stealing Cinderella",
-  //        "artist":"Chuck Wicks",
-  //        "image_small":"https://i.scdn.co/image/d1f58701179fe768cff26a77a46c56f291343d68",
-  //        "image_large":"https://i.scdn.co/image/9ce5ea93acd3048312978d1eb5f6d297ff93375d"
-  //     },
-  //     {
-  //        "title":"Venom - Original Mix",
-  //        "artist":"Ziggy",
-  //        "image_small":"https://i.scdn.co/image/1a4ba26961c4606c316e10d5d3d20b736e3e7d27",
-  //        "image_large":"https://i.scdn.co/image/91a396948e8fc2cf170c781c93dd08b866812f3a"
-  //     },
-  //     {
-  //        "title":"Do It",
-  //        "artist":"Rootkit",
-  //        "image_small":"https://i.scdn.co/image/398df9a33a6019c0e95e3be05fbaf19be0e91138",
-  //        "image_large":"https://i.scdn.co/image/4e47ee3f6214fabbbed2092a21e62ee2a830058a"
-  //     }
-	// ];
-
-	// $scope.currentSong = angular.copy($scope.songs[0]);
+	Recommendations.init()
+    .then(function(){
+      $scope.currentSong = Recommendations.queue[0];
+      // Recommendations.playCurrentSong();
+    });
 
 	$scope.sendFeedback = function(bool){
-		if(bool) User.addSongToFavorites($scope.currentSong);
+		if(bool) { User.addSongToFavorites($scope.currentSong) }
 		$scope.currentSong.rated = bool;
 		$scope.currentSong.hide = true;
 		
@@ -43,9 +21,42 @@ Controller for the discover page
 		$timeout(function() {
 
 			$scope.currentSong = Recommendations.queue[0];
-			
+
 		}, 250);
+
+	  Recommendations.playCurrentSong();
+		
 	}
+
+	$scope.nextAlbumImg = function() {
+    if (Recommendations.queue.length > 1) {
+      return Recommendations.queue[1].image_large;
+    }
+
+    return '';
+  }
+ //  $scope.songs = [
+ // 	  {
+ //         "title":"Stealing Cinderella",
+ //         "artist":"Chuck Wicks",
+ //         "image_small":"https://i.scdn.co/image/d1f58701179fe768cff26a77a46c56f291343d68",
+ //         "image_large":"https://i.scdn.co/image/9ce5ea93acd3048312978d1eb5f6d297ff93375d"
+ //      },
+ //      {
+ //         "title":"Venom - Original Mix",
+ //         "artist":"Ziggy",
+ //         "image_small":"https://i.scdn.co/image/1a4ba26961c4606c316e10d5d3d20b736e3e7d27",
+ //         "image_large":"https://i.scdn.co/image/91a396948e8fc2cf170c781c93dd08b866812f3a"
+ //      },
+ //      {
+ //         "title":"Do It",
+ //         "artist":"Rootkit",
+ //         "image_small":"https://i.scdn.co/image/398df9a33a6019c0e95e3be05fbaf19be0e91138",
+ //         "image_large":"https://i.scdn.co/image/4e47ee3f6214fabbbed2092a21e62ee2a830058a"
+ //      }
+	// ];
+
+	// $scope.currentSong = angular.copy($scope.songs[0]);
 })
 
 
@@ -64,6 +75,11 @@ Controller for the favorites page
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope) {
-
+.controller('TabsCtrl', function($scope, Recommendations) {
+	$scope.enteringFavorites = function() {
+    Recommendations.haltAudio();
+  }
+	$scope.leavingFavorites = function() {
+    Recommendations.init();
+  }
 });
--- code/www/js/services.js
+++ code/www/js/services.js
@@ -7,13 +7,13 @@ angular.module('songhop.services', [])
 	}
 
 	o.addSongToFavorites = function(song) {
-		if (!song) return false;
+		if (!song) {return false};
 
 		o.favorites.unshift(song); 
 	}
 
 	o.removeSongFromFavorites = function(song, index) {
-		if (!song) return false;
+		if (!song) {return false};
 
 		o.favorites.splice(index, 1);
 	}
@@ -22,10 +22,23 @@ angular.module('songhop.services', [])
 
 })
 
-.factory('Recommendations', function($http, SERVER) {
+.factory('Recommendations', function($http, $q, SERVER) {
 	var o = {
 		queue: []
 	};
+	var media;
+
+	o.init = function() {
+    if (o.queue.length === 0) {
+      // if there's nothing in the queue, fill it.
+      // this also means that this is the first call of init.
+      return o.getNextSongs();
+
+    } else {
+      // otherwise, play the current song
+      return o.playCurrentSong();
+    }
+  }
 
 	o.getNextSongs = function() {
 		return $http({
@@ -39,10 +52,33 @@ angular.module('songhop.services', [])
 	o.nextSong = function() {
 		o.queue.shift();
 
+		o.haltAudio();
+
 		if (o.queue.length <= 3) {
 			o.getNextSongs();
 		}
 	}
+
+	 o.playCurrentSong = function() {
+    var defer = $q.defer();
+    console.log(o.queue[0].artist);
+    // play the current song's preview
+    media = new Audio(o.queue[0].preview_url);
+
+    // when song loaded, resolve the promise to let controller know.
+    media.addEventListener("loadeddata", function() {
+      defer.resolve();
+    });
+
+    media.play();
+
+    return defer.promise;
+  }
+
+  // used when switching to favorites tab
+  o.haltAudio = function() {
+    if (media) media.pause();
+  }
 
 	return o;
 
