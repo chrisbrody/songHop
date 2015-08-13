8bffa482bad52baf414287ff45c6d8edde3900a6
Chris Brody <chrisbrody@codifyacademy.com>
2015-08-13 10:32:03 -0400

    Added Loaders to Songs

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -4,27 +4,54 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope, $timeout, User, Recommendations) {
+.controller('DiscoverCtrl', function($scope, $ionicLoading, $timeout, User, Recommendations) {
+
+	// helper functions for loading
+  var showLoading = function() {
+    $ionicLoading.show({
+      template: '<i class="ion-loading-c"></i>',
+      noBackdrop: true
+    });
+  }
+
+  var hideLoading = function() {
+    $ionicLoading.hide();
+  }
+
+  // set loading to true first time while we retrieve songs from server.
+  showLoading();
+
 	Recommendations.init()
     .then(function(){
       $scope.currentSong = Recommendations.queue[0];
-      // Recommendations.playCurrentSong();
+      return Recommendations.playCurrentSong();
+    })
+    .then(function(){
+      // turn loading off
+      hideLoading();
+      $scope.currentSong.loaded = true;
     });
 
 	$scope.sendFeedback = function(bool){
-		if(bool) { User.addSongToFavorites($scope.currentSong) }
-		$scope.currentSong.rated = bool;
-		$scope.currentSong.hide = true;
-		
-		Recommendations.nextSong();
+		// first, add to favorites if they favorited
+    if (bool) User.addSongToFavorites($scope.currentSong);
 
-		$timeout(function() {
+    // set variable for the correct animation sequence
+    $scope.currentSong.rated = bool;
+    $scope.currentSong.hide = true;
 
-			$scope.currentSong = Recommendations.queue[0];
+    // prepare the next song
+    Recommendations.nextSong();
 
-		}, 250);
+    // update current song in scope, timeout to allow animation to complete
+    $timeout(function() {
+      $scope.currentSong = Recommendations.queue[0];
+      $scope.currentSong.loaded = false;
+    }, 250);
 
-	  Recommendations.playCurrentSong();
+    Recommendations.playCurrentSong().then(function() {
+      $scope.currentSong.loaded = true;
+    });
 		
 	}
 
